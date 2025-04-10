e15f0291079fc2fa2d73f309014d4fd14d8e789f
ankita-yodaplus <ankita@yodaplus.com>
2025-04-10 12:26:02 +0530

    component communication

--- Day1_ReactBasics/projectpilot/src/projects/ProjectList.tsx
+++ Day1_ReactBasics/projectpilot/src/projects/ProjectList.tsx
@@ -1,27 +1,40 @@
 import { Project } from './Project';
 import  ProjectCard  from './ProjectCard';
+import { useState } from 'react';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps{
     projects :Project[];
-
+    onSave : (project :Project) => void;
 }
 
-function ProjectList({projects} :ProjectListProps){
+function ProjectList({ projects, onSave } :ProjectListProps){
+
+    const [projectBeingEdited, setProgectBeingEdited ]= useState({});
+
     const handleEdit = (project : Project) =>{
-        console.log(project);
-    }
+        // console.log(project);
+        setProgectBeingEdited(project);
+    };
 
-    const items = projects.map(project =>(
+    const cancelEditing = () =>{
+        setProgectBeingEdited({});
+    };
+
+   return (<div className="row">
+    {projects.map(project =>(
     <div key={project.id} className="col-sm">
-        <ProjectCard 
-        project={project}
-        onEdit={handleEdit}
-        />
+        {project === projectBeingEdited ?(
+            <ProjectForm  
+            onSave={onSave}
+            onCancel={cancelEditing}/>
+        ) :( 
+        <ProjectCard  project={project} onEdit={handleEdit}  />
+        ) }  
     </div>
-))
-
-
-return <div className="row"> {items} </div>;
+  )) }
+   </div>
+  );
 
 }
 export default ProjectList
--- Day1_ReactBasics/projectpilot/src/projects/ProjectPage.tsx
+++ Day1_ReactBasics/projectpilot/src/projects/ProjectPage.tsx
@@ -1,14 +1,24 @@
 import { MOCK_PROJECTS } from "./MockProjects";
 import ProjectList from "./ProjectList";
+import { Project } from "./Project";
+
+
 
 
 
 function ProjectPage(){
+
+    const saveProject = (project :Project) =>{
+        console.log('save project',project)
+    }
+
     return (
        <>
        <h1>Projects</h1>
        {/* <pre>{JSON.stringify(MOCK_PROJECTS,null,' ')}</pre> */}
-       <ProjectList projects={MOCK_PROJECTS} />
+       <ProjectList 
+       onSave={saveProject}
+       projects={MOCK_PROJECTS} />
        </> 
     )
 }
