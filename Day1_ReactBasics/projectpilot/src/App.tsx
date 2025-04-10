f04e21327b0ccee3bc5a886cc0f983bdfa9f92ad
ankita-yodaplus <ankita@yodaplus.com>
2025-04-10 14:42:54 +0530

    Router Basics

--- Day1_ReactBasics/projectpilot/src/App.tsx
+++ Day1_ReactBasics/projectpilot/src/App.tsx
@@ -2,6 +2,9 @@
 
 import './App.css'
 import ProjectPage from './projects/ProjectPage'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router'
+import HomePage from './home/HomePage';
+import logo from './assets/logo.jpeg'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,10 +17,31 @@ function App() {
     //   </blockquote>
     // </>
 
-    <div className='container'>
-      <ProjectPage />
-    </div>
-  )
-}
+    // <div className='container'>
+    //   <ProjectPage />
+    // </div>
+
+    <BrowserRouter>
+      <header className='sticky'>
+        <span className='logo'>
+          <img src={logo} alt='logo' width="49" height="99"/>
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className='icon-home'></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/projects' element={<ProjectPage />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App
