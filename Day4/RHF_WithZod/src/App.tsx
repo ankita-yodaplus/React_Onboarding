44ad7e079e8f393267e41984276f3c6aee27053c
ankita-yodaplus <ankita@yodaplus.com>
2025-04-14 16:57:41 +0530

    RHF-zod

---
+++ Day4/RHF_WithZod/src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+// import ReactHookForm from './components/ReactHookForm'
+import ReactHookFormWithZod from './components/ReactHookFormWithZod'
+// import SimpleForm from './components/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>React Hook form  with zod</h1>
+      {/* <SimpleForm /> */}
+      {/* <ReactHookForm /> */}
+      <ReactHookFormWithZod />
+    </>
+  )
+}
+
+export default App
